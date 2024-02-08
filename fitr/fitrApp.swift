6a99046abcf727a3b7536649d50ee88d52a80a3d
craigSochocki <sochocki.craig@gmail.com>
2024-02-08 12:16:51 -0500

    Initial Commit

---
+++ fitr/fitrApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitrApp.swift
+//  fitr
+//
+//  Created by Craig Sochocki on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct fitrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
